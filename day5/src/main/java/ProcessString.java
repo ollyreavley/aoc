71ce1154c490e778313af20c230583627d93eb3b
Oliver Reavley <oliverreavley@hotmail.com>
2025-01-07 17:24:30 +0000

    corrected arraylist error

--- day5/src/main/java/ProcessString.java
+++ day5/src/main/java/ProcessString.java
@@ -22,11 +22,14 @@ public class ProcessString {
                 String[] printingRules = lines[i].split("\\|");
                 int first = Integer.parseInt(printingRules[0]);
                 List<Integer> subsequents = printingRuleMap.getOrDefault(first, null);
+
                 if(subsequents == null){
-                    List<Integer> second = new ArrayList<>(Integer.parseInt(printingRules[1]));
+                    List<Integer> second = new ArrayList<>();
+                    second.add(Integer.parseInt(printingRules[1]));
                     printingRuleMap.put(first, second);
                 } else{
                     subsequents.add(Integer.parseInt(printingRules[1]));
+                    printingRuleMap.put(first, subsequents);
                 }
             }
             if(lines[i].contains(",")){
@@ -43,21 +46,6 @@ public class ProcessString {
     }
 
     private void checkOrdering(){
-        int sum = 0;
-        for(int i = 0; i < printingOrderList.size(); i++){
-            boolean correct = true;
-            for(int n = 0; n < printingOrderList.get(i).size(); n++){
-                List<Integer> updateList = printingRuleMap.get(printingOrderList.get(i).get(n));
-                for(int p = 0; p < (printingOrderList.get(i).size() - (n + 1)); p++){
-                    if(updateList == null || !updateList.contains(printingOrderList.get(i).get(p))){
-                        correct = false;
-                    }
-                }
-            }
-            if(correct){
-                sum = sum + printingOrderList.get(i).get(printingOrderList.get(i).size() / 2);
-            }
-        }
-        System.out.println(sum);
+        
     }
 }
